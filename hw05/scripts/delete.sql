932dc1da6e97b94b198fe56b013038c2ee21c2fd
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-04-14 21:23:33 +0200

    Implement delete

--- hw05/scripts/delete.sql
+++ hw05/scripts/delete.sql
@@ -2,3 +2,8 @@ DELETE FROM products.products
 USING products.categories
 WHERE categories.id = products.category_id AND
 categories.title = 'Компьютеры';
+
+DELETE FROM products_workers.shoppers
+USING public.addresses
+WHERE addresses.id = shoppers.address_id AND
+addresses.city = 'Novo Hamburgo';
